97d5dba0e25f4b6f2fa20c297a3240a1f360639d
SnatcherSam <119623126+SnatcherSam@users.noreply.github.com>
2025-03-21 19:09:37 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
--- In this SQL file, write (and comment!) the schema of your database, including the CREATE TABLE, CREATE INDEX, CREATE VIEW, etc. statements that compose it
+-- This file contains the initial creation of the database
 
 CREATE TABLE "Athletes" (
     "AthleteID" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
